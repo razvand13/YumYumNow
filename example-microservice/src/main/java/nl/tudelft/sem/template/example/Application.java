0bb8222dee04853190b9c76e66f99294193144b3
AlexisFimeyer <a.h.p.a.fimeyer@student.tudelft.nl>
2023-11-28 14:42:55 +0100

    Test pushing repo

--- example-microservice/src/main/java/nl/tudelft/sem/template/example/Application.java
+++ example-microservice/src/main/java/nl/tudelft/sem/template/example/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Example microservice application.
+ * Example microservice application!
  */
 @SpringBootApplication
 public class Application {
