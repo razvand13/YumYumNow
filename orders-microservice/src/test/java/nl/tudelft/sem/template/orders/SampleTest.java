d210e1e5ae581bb416274237c512c516e757e692
Clayton Stokka <c.c.stokka@student.tudelft.nl>
2023-12-05 15:16:10 +0100

    fix checkstyle problem

--- orders-microservice/src/test/java/nl/tudelft/sem/template/orders/SampleTest.java
+++ orders-microservice/src/test/java/nl/tudelft/sem/template/orders/SampleTest.java
@@ -1,8 +1,8 @@
 package nl.tudelft.sem.template.orders;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
 
 public class SampleTest {
     @Test
