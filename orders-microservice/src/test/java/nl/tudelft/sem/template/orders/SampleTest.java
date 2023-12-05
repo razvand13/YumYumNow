94307e33d5f202f273435df4e19eaf162ce1a095
Clayton Stokka <c.c.stokka@student.tudelft.nl>
2023-12-05 14:37:02 +0100

    Refactor orders microservice test directory and provide a sample test for report generation

---
+++ orders-microservice/src/test/java/nl/tudelft/sem/template/orders/SampleTest.java
@@ -0,0 +1,13 @@
+package nl.tudelft.sem.template.orders;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SampleTest {
+    @Test
+    public void sampleTest() {
+        assertTrue(true);
+
+    }
+}
