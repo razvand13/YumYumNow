0b1aa198542c1cb124ad83446060c4eb39568089
Mate Rodic <m.rodic@student.tudelft.nl>
2024-01-11 16:27:29 +0100

    Checkstyle fix - IOrderService

--- orders-microservice/src/main/java/nl/tudelft/sem/template/orders/domain/IOrderService.java
+++ orders-microservice/src/main/java/nl/tudelft/sem/template/orders/domain/IOrderService.java
@@ -8,8 +8,12 @@ import java.util.UUID;
 
 public interface IOrderService {
     Order findById(UUID orderId);
+
     List<Order> findAll();
+
     Order save(Order order);
+
     void delete(UUID orderId);
+
     double calculateOrderPrice(List<DishEntity> dishEntityList);
 }
