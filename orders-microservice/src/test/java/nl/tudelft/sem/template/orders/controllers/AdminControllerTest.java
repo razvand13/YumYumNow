3655a2b611dc6c87639856df420f351f5f5803ef
Mate Rodic <m.rodic@student.tudelft.nl>
2024-01-11 23:19:57 +0100

    Changed imports to fix checkstyle fail.

--- orders-microservice/src/test/java/nl/tudelft/sem/template/orders/controllers/AdminControllerTest.java
+++ orders-microservice/src/test/java/nl/tudelft/sem/template/orders/controllers/AdminControllerTest.java
@@ -13,7 +13,11 @@ import java.util.List;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verifyNoInteractions;
 
 class AdminControllerTest {
 
